6468c906e5e40c0e7cacba4a4e26fee700b234cb
Piyush <piyushgambhir1910@gmail.com>
2026-02-24 15:34:03 +0530

    Initial demo website upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("signupBtn").addEventListener("click", function() {
+    alert("Signup Button Clicked");
+});
+
+document.getElementById("buyBtn").addEventListener("click", function() {
+    alert("Buy Button Clicked");
+});
